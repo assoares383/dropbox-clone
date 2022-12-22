e05c8d4d15eb04d19e4f42e0188e52cb3265eee3
= <alexandressoares383@gmail.com>
2022-12-22 20:01:58 -0300

    feat: renaming files

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -39,6 +39,18 @@ class DropBoxController {
   }
 
   initEvents() {
+    this.btnRename.addEventListener("click", (e) => {
+      let li = this.getSelection()[0];
+      let file = JSON.parse(li.dataset.file);
+
+      let name = prompt("Renomear o arquivo: ", file.name);
+
+      if (name) {
+        file.name = name;
+        this.getFirebaseRef().child(li.dataset.key).set(file);
+      }
+    });
+
     this.listFilesEl.addEventListener("selectionChange", (e) => {
       switch (this.getSelection().length) {
         case 0:
@@ -333,9 +345,13 @@ class DropBoxController {
   getFileView(file, key) {
     let li = document.createElement("li");
     li.dataset.key = key;
+
+    li.dataset.file = file;
+    li.dataset.file = JSON.stringify(file);
+
     li.innerHTML = `
       ${this.getFileIconView(file)}
-      <div class="name text-center">${file.originalFilename}</div>
+      <div class="name text-center">${file.name}</div>
     `;
 
     this.initEventsLi(li);
